99869e435109d4ea38029d635e96a7a1ad0651cb
Julie Raymond <julie.raymond@pichet.com>
2025-07-22 12:14:40 +0200

    FIX - Authorization - Delete - Removed unused endpoint `/admin/resource` with `DELETE_PRIVILEGE` authority and `ADMIN` role, as it was deemed unnecessary.

--- src/main/java/fr/mossaab/security/controller/AuthorizationController.java
+++ src/main/java/fr/mossaab/security/controller/AuthorizationController.java
@@ -36,20 +36,12 @@ public class AuthorizationController {
         return ResponseEntity.ok("Hello, you have access to a protected resource that requires admin role and read authority.");
     }
 
-    @DeleteMapping("/admin/resource")
-    @PreAuthorize("hasAuthority('DELETE_PRIVILEGE') and hasRole('ADMIN')")
-    public ResponseEntity<String> sayHelloWithRoleAdminAndDeleteAuthority() {
-        return ResponseEntity.ok("Hello, you have access to a protected resource that requires admin role and delete authority.");
-    }
+    
     @PostMapping("/user/resource")
     @PreAuthorize("hasAuthority('WRITE_PRIVILEGE') and hasAnyRole('ADMIN','USER')")
     public ResponseEntity<String> sayHelloWithRoleUserAndCreateAuthority() {
         return ResponseEntity.ok("Hello, you have access to a protected resource that requires user role and write authority.");
     }
-    @PutMapping("/user/resource")
-    @PreAuthorize("hasAuthority('UPDATE_PRIVILEGE') and hasAnyRole('ADMIN','USER')")
-    public ResponseEntity<String> sayHelloWithRoleUserAndUpdateAuthority() {
-        return ResponseEntity.ok("Hello, you have access to a protected resource that requires user role and update authority.");
-    }
+    
 
 }
